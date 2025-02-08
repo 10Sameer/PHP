88be97ab77271e2fc153ed97195dfaf0622db69a
Sameer <bhandarisameer173@gmail.com>
2025-02-08 20:18:44 +0545

    operators

---
+++ assignmentOperators.php
@@ -0,0 +1,30 @@
+<?php
+$x = 20;
+$x += 20;
+echo $x;
+echo "<br>";
+// Outputs: 40
+
+$x = 40;
+$x -= 20;
+echo $x;
+echo "<br>";
+// Outputs: 20
+
+$x = 5;
+$x *= 25; 
+echo $x;
+echo "<br>";
+// Outputs: 125
+
+$x = 50;
+$x /= 10; 
+echo $x;
+echo "<br>";
+// Outputs: 5
+
+$x = 100;
+$x %= 15;
+echo $x;
+// Outputs: 10
+?>
