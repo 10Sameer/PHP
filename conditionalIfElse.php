464069405be99ae96e0a3a5fff6868379cd21bcd
Sameer <bhandarisameer173@gmail.com>
2025-02-11 21:44:41 +0545

    Op

---
+++ conditionalIfElse.php
@@ -0,0 +1,28 @@
+<?php
+$a=10;
+if ($a=3){
+echo"it is lionel Messi";
+}
+echo"<br>";
+
+$a=10;
+if ($a=3){
+echo"it is lionel Messi";
+} else{
+ echo"it is Ankara Messi";
+echo"<br>";
+
+}
+echo"<br>";
+
+$d=date("d");
+if($d=="Fri"){
+    echo"Have a nice day";
+}
+else if($d=="Sun"){
+    echo"Happy holiday";
+}
+else{
+    echo"Boring day.";
+}
+?>
