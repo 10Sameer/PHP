0aa6ea346fc98ccac1658cefb5c484619588d0a8
Sameer <bhandarisameer173@gmail.com>
2025-05-21 23:07:20 +0545

    Multi

--- multidimensionalarray.php
+++ multidimensionalarray.php
@@ -1,12 +1,23 @@
 <?php
- $emp=array(
-     array(1,"Ram",400000),array(2,"Shyam",500000),array(3,"Sita",380000)
-     
- );
- for($row=0;$row<3;$row++){
-  for($col=0;$col<3;$col++){
-     echo $emp [$row] [$col]." ";
+// Creating a multidimensional array
+$students = array(
+    array("Name" => "Alice", "Age" => 20, "Grade" => "A"),
+    array("Name" => "Bob",   "Age" => 22, "Grade" => "B"),
+    array("Name" => "Carol", "Age" => 19, "Grade" => "A+")
+);
+
+// Displaying the array content
+echo "<h3>Student Details</h3>";
+echo "<table border='1' cellpadding='10'>";
+echo "<tr><th>Name</th><th>Age</th><th>Grade</th></tr>";
+
+foreach ($students as $student) {
+    echo "<tr>";
+    echo "<td>" . $student["Name"] . "</td>";
+    echo "<td>" . $student["Age"] . "</td>";
+    echo "<td>" . $student["Grade"] . "</td>";
+    echo "</tr>";
 }
-   echo"\n";
-}
- ?> 
+
+echo "</table>";
+?>
