417aeb46327ac77bbe1895a24b5b7690433b88df
Sameer <bhandarisameer173@gmail.com>
2025-05-21 23:08:19 +0545

    Divide

---
+++ divide.php
@@ -0,0 +1,14 @@
+<?php
+// Define two 2x2 matrices
+$matrix1 = array(
+    array(10, 20),
+    array(30, 40)
+);
+
+$matrix2 = array(
+    array(2, 4),
+    array(5, 8)
+);
+
+// Result matrix
+$result = array();
