edc39193ba7fac19cbacc7c35f81ec8f21be59b5
Sameer <bhandarisameer173@gmail.com>
2025-05-21 23:09:18 +0545

    Add

---
+++ addArray.php
@@ -0,0 +1,17 @@
+<?php
+// Define three 2x2 matrices
+$matrix1 = array(
+    array(1, 2),
+    array(3, 4)
+);
+
+$matrix2 = array(
+    array(5, 6),
+    array(7, 8)
+);
+
+$matrix3 = array(
+    array(9, 10),
+    array(11, 12)
+);
+
