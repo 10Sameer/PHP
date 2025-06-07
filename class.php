7a922abd43b5c42d0f19389e6b7228555007a92e
Sameer <bhandarisameer173@gmail.com>
2025-06-07 18:59:55 +0545

    Car class php

--- class.php
+++ class.php
@@ -4,3 +4,15 @@ class Car {
     public $brand;
     public $color;
 
+    // Constructor
+    public function __construct($brand, $color) {
+        $this->brand = $brand;
+        $this->color = $color;
+    }
+
+    // Method
+    public function honk() {
+        return "Beep! I'm a {$this->color} {$this->brand}.";
+    }
+}
+
