8e372963579ca6cbfaa0db14479c167b0d7d4008
Sameer <bhandarisameer173@gmail.com>
2025-06-07 19:02:07 +0545

    Construster

--- userclass.php
+++ userclass.php
@@ -4,3 +4,10 @@ class User {
     private $username;
     private $email;
     private $isAdmin;
+
+    // Constructor
+    public function __construct($username, $email, $isAdmin = false) {
+        $this->username = $username;
+        $this->email = $email;
+        $this->isAdmin = $isAdmin;
+    }
