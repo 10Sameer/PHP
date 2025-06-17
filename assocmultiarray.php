25f12b87edb24a4bbe65d5ac977bb02f7230dded
Sameer <bhandarisameer173@gmail.com>
2025-06-17 20:27:38 +0545

    associative mulit

--- assocmultiarray.php
+++ assocmultiarray.php
@@ -1,6 +1,19 @@
 <?php
-  $favorites=array( 
-    array("name"=>"Ram","mob"=>"565445464","email"=>"hsajhda@gmail.com")
-  );
-  array("name"=>"Monty","mob"=>"565445464","email"=>"hsajhda@gmail.com")
-?>
+
+$bakery = [
+    "cakes" => [
+        "choco_delight" => [
+            "name" => "Chocolate Delight",
+            "price" => 15.99,
+            "stock" => 12,
+            "ingredients" => ["flour", "cocoa", "sugar", "cream"],
+            "variants" => [
+                "small" => 10.99,
+                "medium" => 15.99,
+                "large" => 20.99
+            ]
+        ],
+ 
+   
+];
+
